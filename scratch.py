53a22be66ba09426201c725bb9762ac1a5890ca7
ravaattar <rava.naufal2014@gmail.com>
2022-01-25 07:53:56 +0700

    found a bunch of keys DONE

--- scratch.py
+++ scratch.py
@@ -16,19 +16,34 @@ col = 0
 
 directions = 8
 
-# key = "EARTH"
-# key = "JUPITER"
-# key = "MARS"
-# key = "MERCURY"
-# key = "NEPTUNE"
-# key = "SATURN"
-# key = "URANUS"
-key = "VENUS"
+# curr_key = "EARTH"
+# curr_key = "JUPITER"
+# curr_key = "MARS"
+# curr_key = "MERCURY"
+# curr_key = "NEPTUNE"
+# curr_key = "SATURN"
+# curr_key = "URANUS"
+# curr_key = "VENUS"
+
+keys_arr = [
+    "EARTH",
+    "JUPITER",
+    "MARS",
+    "MERCURY",
+    "NEPTUNE",
+    "SATURN",
+    "URANUS"
+]
+
 key_constructors = []
+all_key_constructors = []
+
+key_stat = [0 for i in range(len(keys_arr))]
+key_count = 0
 
 found = False
 found_idx = [-1, -1]
-count = 0
+count_comparison = 0
 
 def print_keyword(puzzle, key_constructors):
     for row in range (len(puzzle)):
@@ -39,157 +54,189 @@ def print_keyword(puzzle, key_constructors):
                 print("-", end=" ")
         print()
 
-while (row < puzzle_rows) and (not found):
-    col = 0
-    while (col < puzzle_cols) and (not found):
-        curr_word = puzzle[row][col]    
+def output_program(puzzle, all_key_constructors, keys_arr):
+    for i in range(len(keys_arr)):
+        print(keys_arr[i])
+        for row in range (len(puzzle)):
+            for col in range(len(puzzle[0])):
+                if([row, col] in all_key_constructors[i]):
+                    print(puzzle[row][col], end=" ")
+                else:
+                    print("-", end=" ")
+            print() 
+        print("===")
 
-        dir_state = 0
-        while(dir_state < 8) and (not found):
-            if(dir_state == 0):
-                boundary_length = row + 1
-                if(len(key) <= boundary_length):
-                    finding_idx = 0
-                    while((finding_idx < len(key)) and (key[finding_idx] == puzzle[row-finding_idx][col])):
-                        key_idx = [row-finding_idx, col]
-                        key_constructors.append(key_idx)
+while(not found) and (key_count < len(keys_arr)):
+    curr_key = keys_arr[key_count]
 
-                        finding_idx += 1
-                    
-                    if (finding_idx == len(key)):
-                        found = True
-                        found_idx = [row, col]
-                    else:
-                        key_constructors.clear() 
+    row = 0
+    while (row < puzzle_rows) and (key_stat[key_count] == 0):
+        col = 0
+        while (col < puzzle_cols) and (key_stat[key_count] == 0):
+            # curr_word = puzzle[row][col]    
 
-            elif(dir_state == 1):
-                boundary_length = row + 1
-                x_boundary = puzzle_cols - col 
-                if (x_boundary < boundary_length):
-                    boundary_length = x_boundary
-                if(len(key) <= boundary_length):
-                    finding_idx = 0
-                    while((finding_idx < len(key)) and (key[finding_idx] == puzzle[row-finding_idx][col+finding_idx])):
-                        key_idx = [row-finding_idx, col+finding_idx]
-                        key_constructors.append(key_idx)
-                        
-                        finding_idx += 1
-                    if (finding_idx == len(key)):
-                        found = True
-                        found_idx = [row, col]
-                    else:
-                        key_constructors.clear() 
-            
-            elif(dir_state == 2):
-                boundary_length = puzzle_cols - col 
-                if(len(key) <= boundary_length):
-                    finding_idx = 0
-                    while((finding_idx < len(key)) and (key[finding_idx] == puzzle[row][col+finding_idx])):
-                        key_idx = [row, col+finding_idx]
-                        key_constructors.append(key_idx)
-                        
-                        finding_idx += 1
-                    if (finding_idx == len(key)):
-                        found = True
-                        found_idx = [row, col]
-                    else:
-                        key_constructors.clear() 
-            
-            elif(dir_state == 3):
-                boundary_length = puzzle_cols - col 
-                y_boundary = puzzle_rows - row
-                if(y_boundary < boundary_length):
-                    boundary_length = y_boundary
-                if(len(key) <= boundary_length):
-                    finding_idx = 0
-                    while((finding_idx < len(key)) and (key[finding_idx] == puzzle[row+finding_idx][col+finding_idx])):
-                        key_idx = [row+finding_idx, col+finding_idx]
-                        key_constructors.append(key_idx)
-                        
-                        finding_idx += 1 
-                    if (finding_idx == len(key)):
-                        found = True
-                        found_idx = [row, col]
-                    else:
-                        key_constructors.clear() 
-            
-            elif(dir_state == 4):
-                boundary_length = puzzle_rows - row
-                if(len(key) <= boundary_length):
-                    finding_idx = 0
-                    while((finding_idx < len(key)) and (key[finding_idx] == puzzle[row+finding_idx][col])):
-                        key_idx = [row+finding_idx, col]
-                        key_constructors.append(key_idx)
-                        
-                        finding_idx += 1
-                    if (finding_idx == len(key)):
-                        found = True
-                        found_idx = [row, col]
-                    else:
-                        key_constructors.clear() 
-            
-            elif(dir_state == 5):
-                boundary_length = puzzle_rows - row
-                x_boundary = col + 1
-                if(x_boundary < boundary_length):
-                    boundary_length = x_boundary
-                if(len(key) <= boundary_length):
-                    finding_idx = 0
-                    while((finding_idx < len(key)) and (key[finding_idx] == puzzle[row+finding_idx][col-finding_idx])):
-                        key_idx = [row+finding_idx, col-finding_idx]
-                        key_constructors.append(key_idx)
-                        
-                        finding_idx += 1
-                    if (finding_idx == len(key)):
-                        found = True
-                        found_idx = [row, col]
-                    else:
-                        key_constructors.clear() 
+            dir_state = 0
+            key_constructors = []
+            while(dir_state < 8) and (key_stat[key_count] == 0):
+                if(dir_state == 0):
+                    boundary_length = row + 1
+                    if(len(curr_key) <= boundary_length):
+                        finding_idx = 0
+                        while((finding_idx < len(curr_key)) and (curr_key[finding_idx] == puzzle[row-finding_idx][col])):
+                            key_idx = [row-finding_idx, col]
+                            key_constructors.append(key_idx)
 
-            elif(dir_state == 6):
-                boundary_length = col + 1
-                if(len(key) <= boundary_length):
-                    finding_idx = 0
-                    while((finding_idx < len(key)) and (key[finding_idx] == puzzle[row][col-finding_idx])):
-                        key_idx = [row, col-finding_idx]
-                        key_constructors.append(key_idx)
+                            finding_idx += 1
                         
-                        finding_idx += 1
-                    if (finding_idx == len(key)):
-                        found = True
-                        found_idx = [row, col]
-                    else:
-                        key_constructors.clear() 
+                        if (finding_idx == len(curr_key)):
+                            key_stat[key_count] = 1
+                            found_idx = [row, col]
+                            all_key_constructors.append(key_constructors)
+                        else:
+                            key_constructors.clear() 
 
-            else:
-                boundary_length = row + 1
-                x_boundary = col + 1
-                if(x_boundary < boundary_length):
-                    boundary_length = x_boundary
-                if(len(key) <= boundary_length):
-                    finding_idx = 0
-                    while((finding_idx < len(key)) and (key[finding_idx] == puzzle[row-finding_idx][col-finding_idx])):
-                        key_idx = [row-finding_idx, col-finding_idx]
-                        key_constructors.append(key_idx)
-                        
-                        finding_idx += 1
-                    if (finding_idx == len(key)):
-                        found = True
-                        found_idx = [row, col]
-                    else:
-                        key_constructors.clear() 
+                elif(dir_state == 1):
+                    boundary_length = row + 1
+                    x_boundary = puzzle_cols - col 
+                    if (x_boundary < boundary_length):
+                        boundary_length = x_boundary
+                    if(len(curr_key) <= boundary_length):
+                        finding_idx = 0
+                        while((finding_idx < len(curr_key)) and (curr_key[finding_idx] == puzzle[row-finding_idx][col+finding_idx])):
+                            key_idx = [row-finding_idx, col+finding_idx]
+                            key_constructors.append(key_idx)
+                            
+                            finding_idx += 1
+                        if (finding_idx == len(curr_key)):
+                            key_stat[key_count] = 1
+                            found_idx = [row, col]
+                            all_key_constructors.append(key_constructors)
+                        else:
+                            key_constructors.clear() 
+                
+                elif(dir_state == 2):
+                    boundary_length = puzzle_cols - col 
+                    if(len(curr_key) <= boundary_length):
+                        finding_idx = 0
+                        while((finding_idx < len(curr_key)) and (curr_key[finding_idx] == puzzle[row][col+finding_idx])):
+                            key_idx = [row, col+finding_idx]
+                            key_constructors.append(key_idx)
+                            
+                            finding_idx += 1
+                        if (finding_idx == len(curr_key)):
+                            key_stat[key_count] = 1
+                            found_idx = [row, col]
+                            all_key_constructors.append(key_constructors)
+                        else:
+                            key_constructors.clear() 
+                
+                elif(dir_state == 3):
+                    boundary_length = puzzle_cols - col 
+                    y_boundary = puzzle_rows - row
+                    if(y_boundary < boundary_length):
+                        boundary_length = y_boundary
+                    if(len(curr_key) <= boundary_length):
+                        finding_idx = 0
+                        while((finding_idx < len(curr_key)) and (curr_key[finding_idx] == puzzle[row+finding_idx][col+finding_idx])):
+                            key_idx = [row+finding_idx, col+finding_idx]
+                            key_constructors.append(key_idx)
+                            
+                            finding_idx += 1 
+                        if (finding_idx == len(curr_key)):
+                            key_stat[key_count] = 1
+                            found_idx = [row, col]
+                            all_key_constructors.append(key_constructors)
+                        else:
+                            key_constructors.clear() 
+                
+                elif(dir_state == 4):
+                    boundary_length = puzzle_rows - row
+                    if(len(curr_key) <= boundary_length):
+                        finding_idx = 0
+                        while((finding_idx < len(curr_key)) and (curr_key[finding_idx] == puzzle[row+finding_idx][col])):
+                            key_idx = [row+finding_idx, col]
+                            key_constructors.append(key_idx)
+                            
+                            finding_idx += 1
+                        if (finding_idx == len(curr_key)):
+                            key_stat[key_count] = 1
+                            found_idx = [row, col]
+                            all_key_constructors.append(key_constructors)
+                        else:
+                            key_constructors.clear() 
+                
+                elif(dir_state == 5):
+                    boundary_length = puzzle_rows - row
+                    x_boundary = col + 1
+                    if(x_boundary < boundary_length):
+                        boundary_length = x_boundary
+                    if(len(curr_key) <= boundary_length):
+                        finding_idx = 0
+                        while((finding_idx < len(curr_key)) and (curr_key[finding_idx] == puzzle[row+finding_idx][col-finding_idx])):
+                            key_idx = [row+finding_idx, col-finding_idx]
+                            key_constructors.append(key_idx)
+                            
+                            finding_idx += 1
+                        if (finding_idx == len(curr_key)):
+                            key_stat[key_count] = 1
+                            found_idx = [row, col]
+                            all_key_constructors.append(key_constructors)
+                        else:
+                            key_constructors.clear() 
 
-            dir_state += 1
-        col += 1
-    row += 1
+                elif(dir_state == 6):
+                    boundary_length = col + 1
+                    if(len(curr_key) <= boundary_length):
+                        finding_idx = 0
+                        while((finding_idx < len(curr_key)) and (curr_key[finding_idx] == puzzle[row][col-finding_idx])):
+                            key_idx = [row, col-finding_idx]
+                            key_constructors.append(key_idx)
+                            
+                            finding_idx += 1
+                        if (finding_idx == len(curr_key)):
+                            key_stat[key_count] = 1
+                            found_idx = [row, col]
+                            all_key_constructors.append(key_constructors)
+                        else:
+                            key_constructors.clear() 
+
+                else:
+                    boundary_length = row + 1
+                    x_boundary = col + 1
+                    if(x_boundary < boundary_length):
+                        boundary_length = x_boundary
+                    if(len(curr_key) <= boundary_length):
+                        finding_idx = 0
+                        while((finding_idx < len(curr_key)) and (curr_key[finding_idx] == puzzle[row-finding_idx][col-finding_idx])):
+                            key_idx = [row-finding_idx, col-finding_idx]
+                            key_constructors.append(key_idx)
+                            
+                            finding_idx += 1
+                        if (finding_idx == len(curr_key)):
+                            key_stat[key_count] = 1
+                            found_idx = [row, col]
+                            all_key_constructors.append(key_constructors)
+                        else:
+                            key_constructors.clear() 
+
+                dir_state += 1
+            col += 1
+        row += 1
+
+    if(0 not in key_stat):
+        found = True
+    key_count += 1
 
 print("===")
-print(f"Hasil ditemukan: {found}, dengan starting point ({found_idx[0]}, {found_idx[1]})")
+# print(f"Hasil ditemukan: {found}, dengan starting point ({found_idx[0]}, {found_idx[1]})")
+print(f"Hasil ditemukan: {found}")
 print("===")
-print(f"Penyusun kata: {key_constructors}")
+# print(f"Penyusun kata: {key_constructors}")
+output_program(puzzle, all_key_constructors, keys_arr)
 print("===")
-print_keyword(puzzle, key_constructors)
-print("===")
+
+# print_keyword(puzzle, key_constructors)
+# print("===")
 
     # for row in range (puzzle_rows):
     #     for col in range (puzzle_cols):
